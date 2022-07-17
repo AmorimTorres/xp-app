959a7578928ecc10ec093ff3ef7ecbabac87e307
André <amorimtorresandre@gmail.com>
2022-07-17 14:29:39 -0300

    FEAT: create page account with Header renderization and route /conta

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Account from './Pages/Account';
 import Login from './Pages/Login';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/conta" element={<Account />} />
       </Routes>
     </Router>
   );
---
+++ src/Pages/Account.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Components/Header';
+
+function Account() {
+  return (
+    <div>
+      <Header />
+      <h1> Conta XP </h1>
+    </div>
+  );
+}
+
+export default Account;
