03b60859d14a255c8633739920413f22a2df19c0
André <amorimtorresandre@gmail.com>
2022-07-17 18:17:01 -0300

    FEAT: sharesReducer on store Reducer

--- src/Redux/Store/store.js
+++ src/Redux/Store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../Slicers/user.slicer';
 import accountReducer from '../Slicers/account.slicer';
+import stockReducer from '../Slicers/shares.slicer';
 
 export default configureStore({
   reducer: {
     user: userReducer,
     account: accountReducer,
+    stock: stockReducer,
   },
 });
