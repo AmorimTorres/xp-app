84c7d32dad58262546c7912d2af4d2300af01e6b
André <amorimtorresandre@gmail.com>
2022-07-20 19:06:07 -0300

    REFAC: remove account button

--- src/Pages/Shares/Shares.jsx
+++ src/Pages/Shares/Shares.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import Header from '../../Components/Header/Header';
 import MarketShares from '../../Components/StockMarket/StockMarket';
-import UserShares from '../../Components/UserShares';
+import UserShares from '../../Components/UserShares/UserShares';
 import * as C from './styles';
 
 function Shares() {
@@ -12,11 +11,6 @@ function Shares() {
         <Header />
         <UserShares />
         <MarketShares />
-        <NavLink to="/conta">
-          <button className="account-button" type="button" aria-label="go-to-account-button">
-            Depósito/Retirada
-          </button>
-        </NavLink>
       </C.Content>
     </C.Container>
   );
